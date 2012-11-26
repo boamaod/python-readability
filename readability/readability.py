cf955c2540df6fb90bc297516f0da0755193b340
Travis Truett <travis.truett@akimbo.io>
2012-11-26 09:13:51 -0500

    Added some custom divs/ids for readability to filter out

--- readability/readability.py
+++ readability/readability.py
@@ -23,10 +23,10 @@ log = logging.getLogger()
 # Moved |media| from negativeRe to positiveRe
 # Our custom classes are in front
 REGEXES = {
-    'unlikelyCandidatesRe': re.compile('pageinfo|hidden|cnn_strylceclbtn|cnn_stryshrwdgtbtm|slashpop|feedburner|combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|tweet|twitter', re.I),
+    'unlikelyCandidatesRe': re.compile('android-banner|pageinfo|hidden|cnn_strylceclbtn|cnn_stryshrwdgtbtm|slashpop|feedburner|combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|tweet|twitter', re.I),
     'okMaybeItsACandidateRe': re.compile('and|article|body|column|main|shadow', re.I),
     'positiveRe': re.compile('article|body|content|entry|hentry|main|media|page|pagination|post|text|blog|story', re.I),
-    'negativeRe': re.compile('combx|comment|com-|contact|foot|footer|footnote|masthead|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget', re.I),
+    'negativeRe': re.compile('combx|comment|comments|com-|contact|foot|footer|footnote|masthead|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget', re.I),
     'divToPElementsRe': re.compile('<(a|blockquote|dl|div|img|ol|p|pre|table|ul)', re.I),
     #'replaceBrsRe': re.compile('(<br[^>]*>[ \n\r\t]*){2,}',re.I),
     #'replaceFontsRe': re.compile('<(\/?)font[^>]*>',re.I),
