9a48a48c7da39cac89777b69881f255b6a29f6f2
Dustin Cannon <dustin.cannon@akimbo.io>
2012-12-28 18:29:07 +0000

    Base 64 encode image data
    * Base 64 encode image data and put it in the 'data' field
    * Added 'format' field
    * Bumped akimbo version to 0.1.03

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="readability-lxml",
-    version="0.1.02", # Akimbo Specific Version
+    version="0.1.03", # Akimbo Specific Version
     author="Yuri Baburov",
     author_email="burchik@gmail.com",
     description="fast python port of arc90's readability tool",
