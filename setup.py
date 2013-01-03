6a3ace6cc1ccf8f92dd28e380cb67a7b64ee6437
Dustin Cannon <dustin.cannon@akimbo.io>
2013-01-03 11:58:37 +0000

    Bump version to 0.1.05

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="readability-lxml",
-    version="0.1.04", # Akimbo Specific Version
+    version="0.1.05", # Akimbo Specific Version
     author="Yuri Baburov",
     author_email="burchik@gmail.com",
     description="fast python port of arc90's readability tool",
